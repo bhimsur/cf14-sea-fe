7672d021816d6919070d86af9e5e72497b8fe46e
Bhimantoro Suryo Admodjo <bhimantoros@gmail.com>
2022-07-06 14:10:27 -0400

    Feature/sorting (#1)
    * add ordering
    
    * enhance some feature, remove unused
    
    Co-authored-by: bhimsur <bhimsur>

--- src/view/pages/Home.js
+++ src/view/pages/Home.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import ProductService from "../../service/product.service";
 import ProductCard from "../components/Product.Card";
 import {Col, Row} from "react-bootstrap";
+import orderBy from "lodash/sortBy";
 
 const Home = (props) => {
 	const {onUpdate} = props;
@@ -13,6 +14,7 @@ const Home = (props) => {
 		ProductService.getAll()
 			.then(response => {
 				setProduct(response.data.result);
+				// sortProduct(sortType);
 			})
 			.catch(e => console.log(e));
 	};
@@ -23,22 +25,53 @@ const Home = (props) => {
 		onUpdate();
 	};
 
+	const sortProduct = type => {
+		const types = {
+			nameAsc: "productName",
+			nameDesc: "productName",
+			dateAsc: "createDate",
+			dateDesc: "createDate",
+		};
+		const sortProperty = types[type];
+		let sorted;
+		if (type.includes("name")) {
+			sorted = orderBy([...product], [p => p[sortProperty].toLowerCase()], ["asc"]);
+		} else {
+			sorted = orderBy([...product], [p => p[sortProperty]], ["asc"]);
+		}
+		if (type.includes("Desc")) {
+			sorted = sorted.reverse();
+		}
+		console.log(sorted);
+		setProduct(sorted);
+	};
+
 	return (
-		<Row xs={1} md={2} lg={3} className="g-4">
-			{product && product.map((data, index) => (
-				<Col sm key={data.id}>
-					<ProductCard
-						index={index}
-						id={data.id}
-						productName={data.productName}
-						productImage={data.productImage}
-						price={data.price}
-						description={data.description}
-						onBuy={onBuy}
-					/>
-				</Col>
-			))}
-		</Row>
+		<div>
+			<div className="py-3 d-flex ml-auto align-items-end flex-column">
+				<select onChange={e => sortProduct(e.target.value)}>
+					<option value="nameAsc">A-Z</option>
+					<option value="nameDesc">Z-A</option>
+					<option value="dateAsc">Newest</option>
+					<option value="dateDesc">Oldest</option>
+				</select>
+			</div>
+			<Row xs={1} md={2} lg={3} className="g-4">
+				{product && product.map((data, index) => (
+					<Col sm key={data.id}>
+						<ProductCard
+							index={index}
+							id={data.id}
+							productName={data.productName}
+							productImage={data.productImage}
+							price={data.price}
+							description={data.description}
+							onBuy={onBuy}
+						/>
+					</Col>
+				))}
+			</Row>
+		</div>
 	)
 };
 export default Home;
